323881aa8e5373eb82194b84e702868bca8f8be8
Ig Satan Op <70173542+Satan-tg@users.noreply.github.com>
2023-09-19 23:51:27 +0530

    @Hacker_Jr 🐬

--- utils/approve.py
+++ utils/approve.py
@@ -3,7 +3,7 @@ import asyncio, random
 from pyrogram import Client, filters, errors
 from pyrogram.types import Message, User, InlineKeyboardMarkup, InlineKeyboardButton, ChatJoinRequest
 
-TEXT = ("<b>Hello 👋 {mention}\n\nYour Request To Join {title} Was Approved 🔥.</b>\n\n<b>•➤ JOIN:- @Team_KL -|- @KL_GROUP1</b>")
+TEXT = ("<b>Hello 👋 {mention}\n\nYour Request To Join {title} Was Approved 🔥</b>")
 
 @Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
 async def approve(client, message: ChatJoinRequest):
@@ -12,9 +12,7 @@ async def approve(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)  
     buttons = [[
-                InlineKeyboardButton('🔮 Jᴏɪɴ Mᴏᴠɪᴇs Gʀᴏᴜᴘ 🔮', url='https://t.me/KL_GROUP1')
-              ],[       
-                InlineKeyboardButton('💥 Jᴏɪɴ Mᴏᴠɪᴇs Cʜᴀɴɴᴇʟ 💥', url='https://t.me/Team_KL')
+                InlineKeyboardButton('🔮 Jᴏɪɴ Mᴏᴠɪᴇs Gʀᴏᴜᴘ 🔮', url='https://t.me/CinemaXpressTM')
             ]]
     await client.send_message(chat_id=message.from_user.id, 
         text=TEXT.format(mention=user.mention, title=chat.title),
